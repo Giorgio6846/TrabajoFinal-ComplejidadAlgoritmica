00c5dcd3e2b0621594396e2cf574911e58a8db11
Giorgio6846 <6852733+Giorgio6846@users.noreply.github.com>
2023-10-16 22:25:55 -0500

    Added Map File

---
+++ TrabajoFinal/test.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+from PIL import ImageTk, Image
+import pyvips
+
+root = tk.Tk()
+        
+root.title('Proyecto - Complejidad Algoritmica - (2023 - 2) - Grupo 5 - Seccion WS6D')
+
+#Layout
+root.geometry("1280x720")
+root.config(background='grey')
+        
+root.mainloop()
