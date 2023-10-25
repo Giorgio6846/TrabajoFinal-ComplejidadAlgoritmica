9eb2c84f573f371d170c03ae4576e77a18ab1f88
Giorgio6846 <6852733+Giorgio6846@users.noreply.github.com>
2023-10-25 13:44:26 -0500

    Added ZeroMQ Support

--- Server/server.py
+++ Server/server.py
@@ -1,17 +1,33 @@
 import zerorpc
 import gevent, signal
 
-class PythermalApi: 
-    def echo(self,text):
+from test import calc as real_calc  
+
+class CalcApi(object):
+    def calc(self, text):
+        """based on the input text, return the int result"""
+        try:
+            return real_calc(text)
+        except Exception as e:
+            return 0.0    
+    def echo(self, text):
+        """echo any text"""
         return text
 
-port = 42690
-address = 'tcp://127.0.0.1:' + port
-s = zerorpc.Server(PythermalApi())
-s.bind(address)
+def parse_port():
+    port = 4242
+    try:
+        port = int(sys.argv[1])
+    except Exception as e:
+        pass
+    return '{}'.format(port)
 
-gevent.signal(signal.SIGTERM, s.stop)
-gevent.signal(signal.SIGINT, s.stop)
+def main():
+    addr = 'tcp://127.0.0.1:' + parse_port()
+    s = zerorpc.Server(CalcApi())
+    s.bind(addr)
+    print('start running on {}'.format(addr))
+    s.run()
 
-s.run()
-
+if __name__ == '__main__':
+    main()
