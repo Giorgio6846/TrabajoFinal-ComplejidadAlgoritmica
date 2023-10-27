6c13a901acb9c29ffdcd74362db3f8e946f13206
Giorgio6846 <6852733+Giorgio6846@users.noreply.github.com>
2023-10-27 07:11:45 -0500

    Minor Changes

--- Server/server.py
+++ Server/server.py
@@ -1,33 +1,12 @@
-import zerorpc
-import gevent, signal
+import zmq
 
-from test import calc as real_calc  
+context = zmq.Context()
+socket = context.socket(zmq.REP)
 
-class CalcApi(object):
-    def calc(self, text):
-        """based on the input text, return the int result"""
-        try:
-            return real_calc(text)
-        except Exception as e:
-            return 0.0    
-    def echo(self, text):
-        """echo any text"""
-        return text
+socket.bind("tcp://0.0.0.0:5555")
 
-def parse_port():
-    port = 4242
-    try:
-        port = int(sys.argv[1])
-    except Exception as e:
-        pass
-    return '{}'.format(port)
-
-def main():
-    addr = 'tcp://127.0.0.1:' + parse_port()
-    s = zerorpc.Server(CalcApi())
-    s.bind(addr)
-    print('start running on {}'.format(addr))
-    s.run()
-
-if __name__ == '__main__':
-    main()
+while True:
+    message = socket.recv_json()
+    print("Received Request: ", message)
+    ret = {"a": 123, "b":321}
+    socket.send_json(ret)
