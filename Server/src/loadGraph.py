27087a88e57a624613d8edf565a8b80486c5d3d6
Giorgio6846 <6852733+Giorgio6846@users.noreply.github.com>
2023-10-28 12:44:48 -0500

    Distrito Support

--- Server/src/loadGraph.py
+++ Server/src/loadGraph.py
@@ -1,29 +1,35 @@
 import pandas as pd
-import networkx as nx
 
 nombrePais = "Pais.Peru"
 
 df = pd.read_csv("../Generador/Dataframe.csv", index_col = False)
 df = df.drop(columns=["Unnamed: 0"])
-df
 
-def loadGraph(Grafo):
-    Grafo.add_node(nombrePais)    
-
-    accessDepartamento(Grafo)
+def accessDireccion(dataframeDistrito, Grafo, idDistrito):
+    for index, row in dataframeDistrito.iterrows():        
+            direccion = row["direccion"]
+            
+            Grafo.add_node(direccion, **row.to_dict())
+            #Grafo.nodes[direccion].update(row.to_dict())
+            
+            Grafo.add_edge(idDistrito, direccion)
+            
+def accessDistrito(dfProvincia, Grafo, provincia):
+    Distritos = list(dfProvincia["distrito"].unique())
     
-def accessDepartamento(Grafo):
-    Departamentos = list(df["departamento"].unique())
-    
-    for departamento in Departamentos:
-        dfDepartamento = df.iloc[list(df["departamento"] == departamento)]
-        nombreDepartamento = "Departamento." + str(departamento)
+    for distrito in Distritos: 
+        dfDistrito = dfProvincia.iloc[list(dfProvincia["distrito"] == distrito)]
+                
+        nombreProvincia = "Provincia." + str(provincia)
+        nombreDistrito = "Distrito." + str(distrito)
+        idDistrito = "Distrito." + str(provincia) + "." + str(distrito)
         
-        Grafo.add_node(nombreDepartamento)
-        Grafo.add_edge(nombrePais, nombreDepartamento)
+        Grafo.add_node(idDistrito, **{"Label": nombreDistrito, "provincia": provincia})
+        #Grafo.nodes[nombreDistrito]['provincia'].update(provincia)
         
-        accessProvincia(dfDepartamento, Grafo, departamento)
-
+        Grafo.add_edge(nombreProvincia, idDistrito)
+        accessDireccion(dfDistrito, Grafo, idDistrito)
+        
 def accessProvincia(dfDepartamento, Grafo, departamento):
     Provincias = list(dfDepartamento["provincia"].unique())
     
@@ -33,29 +39,26 @@ def accessProvincia(dfDepartamento, Grafo, departamento):
         nombreDepartamento = "Departamento." + str(departamento)
         nombreProvincia = "Provincia." + str(provincia)
         
-        Grafo.add_node(nombreProvincia)
+        Grafo.add_node(nombreProvincia, **{"Label": nombreProvincia, "departamento": departamento})
+        
         Grafo.add_edge(nombreDepartamento, nombreProvincia)
         
         accessDistrito(dfProvincia, Grafo, provincia)
-
-def accessDistrito(dfProvincia, Grafo, provincia):
-    Distritos = list(dfProvincia["distrito"].unique())
+        
+def accessDepartamento(Grafo):
+    Departamentos = list(df["departamento"].unique())
     
-    for distrito in Distritos: 
-        dfDistrito = dfProvincia.iloc[list(dfProvincia["distrito"] == distrito)]
-                
-        nombreProvincia = "Provincia." + str(provincia)
-        nombreDistrito = "Distrito." + str(distrito)
+    for departamento in Departamentos:
+        dfDepartamento = df.iloc[list(df["departamento"] == departamento)]
+        nombreDepartamento = "Departamento." + str(departamento)
         
-        Grafo.add_node(nombreDistrito)
-        Grafo.add_edge(nombreProvincia, nombreDistrito)
-        accessDireccion(dfDistrito, Grafo, distrito)
-
-def accessDireccion(dataframeDistrito, Grafo, distrito):
-    for index, row in dataframeDistrito.iterrows():
-            nombreDistrito = "Distrito." + str(distrito)
+        Grafo.add_node(nombreDepartamento)
         
-            direccion = row["direccion"]
-            Grafo.add_node(direccion)
-            Grafo.nodes[direccion].update(row.to_dict())
-            Grafo.add_edge(nombreDistrito, direccion)
+        Grafo.add_edge(nombrePais, nombreDepartamento)
+        
+        accessProvincia(dfDepartamento, Grafo, departamento)
+        
+def loadGraph(Grafo):
+    Grafo.add_node(nombrePais)
+    
+    accessDepartamento(Grafo)
