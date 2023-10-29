6619113db5502196e2489fe26a42cbe6687a5740
Giorgio6846 <6852733+Giorgio6846@users.noreply.github.com>
2023-10-29 02:45:52 -0500

    Server Feature

---
+++ Server/src/tools.py
@@ -0,0 +1,57 @@
+import re
+
+def removeDepartamentoString(departamento):
+    departamento = departamento.replace("Departamento.","")
+    departamento = str.capitalize(departamento)
+    return departamento
+
+def removeProvinciaString(provincia):
+    provincia = provincia.replace("Provincia.","")
+    provincia = str.capitalize(provincia)
+    return provincia
+
+def removeDistritoString(distrito):
+    distrito = distrito.replace("Distrito.","")
+    distrito = str.capitalize(distrito)
+    return distrito
+
+def addDepartamentoString(departamento):
+    departamentoTMP = "Departamento." + str.upper(departamento)
+    return departamentoTMP
+
+def addProvinciaString(provincia):
+    provinciaTMP = "Provincia." + str.upper(provincia)
+    return provinciaTMP
+
+def addDistritoString(distrito):
+    distritoTMP = "Distrito." + str.upper(distrito)
+    return distritoTMP
+
+def getTipoNodo(string):
+    if re.search("Departamento.", string):
+        return "Dep"
+    elif re.search("Provincia.", string):
+        return "Prov"
+    elif re.search("Distrito..", string):
+        return "Dis"
+    
+def saveDepartamentosJSON(listDepartamentos):
+    listDepartamentosTMP = []
+    for departamento in listDepartamentos:
+        departamento = removeDepartamentoString(departamento)
+        listDepartamentosTMP.append(departamento)
+    return listDepartamentosTMP
+
+def saveProvinciasJSON(listProvincias):
+    listProvinciasTMP = []
+    for provincia in listProvincias:
+        provincia = removeProvinciaString(provincia)
+        listProvinciasTMP.append(provincia)
+    return listProvinciasTMP
+
+def saveDistritosJSON(listDistritos):
+    listDistrtosTMP = []
+    for distrito in listDistritos:
+        distrito = removeDistritoString(distrito)
+        listDistrtosTMP.append(distrito)
+    return listDistrtosTMP
