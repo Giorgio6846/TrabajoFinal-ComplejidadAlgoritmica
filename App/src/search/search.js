4fc038c49daac5449ab03532bec613ce7c6a204e
Giorgio6846 <6852733+Giorgio6846@users.noreply.github.com>
2023-10-30 12:12:03 -0500

    Added Functions to Select

--- App/src/search/search.js
+++ App/src/search/search.js
@@ -1,46 +1,90 @@
+var listaDepartamentos = []
+var listaProvincias = []
+var listaDistritos = []
+
 async function requestDataServer(requestData){
    requestJSON = JSON.stringify(requestData)
    receivedJSON = await run(requestJSON)
    receivedJSON = JSON.parse(receivedJSON)
 
-   console.log(receivedJSON["departamentos"])
-
-   listaDepartamentos = []
-
-   var selectDepartamento = document.getElementById("selectDepartamento")
-
-   for (i = 0; i < receivedJSON["departamentos"].length; i++)
-   {
-      var departamentoTMP = document.createElement("option");
-      departamentoTMP.text = receivedJSON["departamentos"][i]
-      console.log(receivedJSON["departamentos"][i])
-      selectDepartamento.add(departamentoTMP)
-      listaDepartamentos.push(departamentoTMP)
-   }
-
    return receivedJSON
 }
 
-function requestDepartamentos(){
-   requestDepartamentoData = {
+async function requestDepartamentos(){
+   requestJSONData = {
       "type": "listDepartamento",
       "Departamento": "NULL",
       "Provincia": "NULL",
       "Distrito": "NULL",
    };
 
-   requestDataServer(requestDepartamentoData)
+   JSONdata = await requestDataServer(requestJSONData)
+
+
+   console.log(JSONdata["departamentos"])
+
+   var selectDepartamento = document.getElementById("selectDepartamento")
+
+   /*
+   for (i = 0; i < JSONdata["departamentos"].length; i++) {
+      var departamentoTMP = document.createElement("option");
+      departamentoTMP.text = JSONdata["departamentos"][i]
+      console.log(JSONdata["departamentos"][i])
+      selectDepartamento.add(departamentoTMP)
+      listaDepartamentos.push(departamentoTMP)
+   }
+   */
+   addItemsToSelect(selectDepartamento, listaDepartamentos, JSONdata["departamentos"])
+}
+
+function resetDepartamento(){
+   for(var items in listaDepartamentos)
+   {
+      listaDepartamentos.remove()
+      console.log(items)
+   }
 }
 
 function requestProvincia() {
+   //Se cambia el departamento que depende del seleccionado
+   requestJSONData = {
+      "type": "listDepartamento",
+      "Departamento": "NULL",
+      "Provincia": "NULL",
+      "Distrito": "NULL",
+   };
+
+   var selectProvincia = document.getElementById("selectProvincia")
 
 }
 
 function requestDistrito() {
+   //Se cambia el departamento y provincia que depende del seleccionado
+   requestJSONData = {
+      "type": "listDepartamento",
+      "Departamento": "NULL",
+      "Provincia": "NULL",
+      "Distrito": "NULL",
+   };
 
+   var selectDistrito = document.getElementById("selectDistrito")
+
+}
+
+function addItemsToSelect(selectItem, listSelect, JSONData){
+   for (var i = 0; i < JSONData.length; i++)
+   {
+      var optionTMP = document.createElement("option");
+      optionTMP.text = JSONData[i]
+      console.log(JSONData[i])
+      selectItem.add(optionTMP)
+      listSelect.push(optionTMP)
+   }
 }
 
 var provinciaId = document.getElementById("seccionProvincia");
 var distritoId = document.getElementById("seccionDistrito");
 
 requestDepartamentos()
+
+resetDepartamento()
