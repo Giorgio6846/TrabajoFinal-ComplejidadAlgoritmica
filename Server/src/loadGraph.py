dc9a4ed39404a1e4ba34bd75066e84f872e5276e
Giorgio6846 <6852733+Giorgio6846@users.noreply.github.com>
2023-11-03 18:54:58 -0500

    Fixed File Error

--- Server/src/loadGraph.py
+++ Server/src/loadGraph.py
@@ -1,10 +1,20 @@
 import pandas as pd
+import platform
 
 nombrePais = "Pais.Peru"
 
-#df = pd.read_csv("/Assets/Dataframe.csv", index_col= False)
-df = pd.read_csv("../Generador/Dataframe.csv", index_col = False)
+if platform.system() == 'Windows':
+    filePath = "../Generador/Dataframe.csv"
+elif platform.system() == 'Darwin':
+    filePath = "Generador/Dataframe.csv"
+else:
+    filePath = "Generador/Dataframe.csv"
+    
+df = pd.read_csv(filePath, index_col = False)
 df = df.drop(columns=["Unnamed: 0"])
+
+    
+#df = pd.read_csv("/Assets/Dataframe.csv", index_col= False)
 
 def accessDireccion(dataframeDistrito, Grafo, idDistrito):
     for index, row in dataframeDistrito.iterrows():        
