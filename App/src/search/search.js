e98a073865e4c8324f33356ce0e422e3cf00de4d
MathiasHv18 <mathiashv2014@gmail.com>
2023-11-17 23:33:58 -0500

    charts

--- App/src/search/search.js
+++ App/src/search/search.js
@@ -43,6 +43,11 @@ async function requestDatos(){
    let table = document.querySelector("table")
    clearTable(table)
    generateTable(table, JSONdata["listCalles"])
+
+   graficoVivienda = updateChart(JSONdata["listStats"][0]["statsViviendas"], 'chartViviendas', 'Estadisticas de Viviendas', graficoVivienda);
+   graficoParedes = updateChart(JSONdata["listStats"][0]["statsParedes"], 'chartParedes', 'Estadisticas de Paredes', graficoParedes);
+   graficoTechos = updateChart(JSONdata["listStats"][0]["statsTecho"], 'chartTecho', 'Estadisticas de Techo', graficoTechos);
+   graficoPisos = updateChart(JSONdata["listStats"][0]["statsPiso"], 'chartPiso', 'Estadisticas de Piso', graficoPisos);
 }
 
 function generateTable(table, data){
@@ -61,5 +66,67 @@ function clearTable(table){
       table.deleteRow(i)
    }
 }
+
+
+// Calcular porcentajes
+function calcularPorcentajes(stats) {
+    const total = Object.values(stats).reduce((acc, val) => acc + val, 0);
+    const porcentajes = {};
+    for (const key in stats) {
+        porcentajes[key] = ((stats[key] / total) * 100).toFixed(2);
+    }
+    return porcentajes;
+}
+
+// Configurar y actualizar el gráfico
+function updateChart(stats, chartId, chartLabel, myChart) {
+   const porcentajes = calcularPorcentajes(stats);
+
+   const ctx = document.getElementById(chartId).getContext('2d');
+
+   // Eliminar el gráfico existente si hay uno
+   if (myChart) {
+       myChart.destroy();
+   }
+
+   // Crear un nuevo gráfico con datos actualizados
+   return new Chart(ctx, {
+       type: 'bar',
+       data: {
+           labels: Object.keys(porcentajes),
+           datasets: [{
+               label: chartLabel,
+               data: Object.values(porcentajes),
+               backgroundColor: [
+                   'rgba(255, 99, 132, 0.2)',
+                   'rgba(54, 162, 235, 0.2)',
+                   'rgba(255, 206, 86, 0.2)',
+                   // Agrega más colores si hay más datos
+               ],
+               borderColor: [
+                   'rgba(255, 99, 132, 1)',
+                   'rgba(54, 162, 235, 1)',
+                   'rgba(255, 206, 86, 1)',
+                   // Agrega más colores si hay más datos
+               ],
+               borderWidth: 1
+           }]
+       },
+       options: {
+           scales: {
+               y: {
+                   beginAtZero: true
+               }
+           }
+       }
+   });
+} 
+
+
+let graficoVivienda;
+let graficoTechos;
+let graficoParedes;
+let graficoPisos;
+// Actualizar el gráfico según la categoría seleccionada
 
 requestDatos()
